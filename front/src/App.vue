d55696f29d7d5ee0681a1d7210cc57b0d30c45c5
stdevAdrianPaez <adriantpaez@gmail.com>
2021-05-11 08:29:50 -0400

    Add basic front

---
+++ front/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Search />
+    <div v-if="results.length > 0" class="mt-3 mx-5">
+      <h2>Resultados</h2>
+      <div v-for="id in results" :key="id">
+        <Document :id="id" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import Document from "./components/Document.vue";
+
+export default {
+  name: "App",
+  components: {
+    Search,
+    Document,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mx-5" style="text-align: center">
+    <b-form-input
+      class="mb-3"
+      v-model="query"
+      placeholder="Realice una busqueda..."
+    ></b-form-input>
+    <b-button variant="outline-primary">Buscar</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
